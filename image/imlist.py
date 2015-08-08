1f7965349a001e802cd5cc3f61fefca591fca594
notmikeb <notmikeb@gmail.com>
2015-08-08 09:58:21 +0800

    Update imlist.py
    print out the warning message if not found any file

--- image/imlist.py
+++ image/imlist.py
@@ -3,6 +3,10 @@
 import os
 
 filelist = os.listdir()
+found = 0
 for i in filelist:
     if os.path.splitext(i)[1].upper() == '.JPG':
        print(" found " , i )
+       found = 1
+if found == 0:
+    print (" Not found any *.jpg files ")
